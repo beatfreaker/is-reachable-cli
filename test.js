233a2517932ddcceccabaabbf03dddd087b85a92
beatfreaker <radiachints1512@gmail.com>
2015-08-25 21:28:30 +0530

    Done<F6

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 'use strict';
 var test = require('tape');
 var childProcess = require('child_process');
+var logSymbols = require('log-symbols');
 
 test('Testing output', function (t) {
-	t.plan(2);
+	t.plan(1);
 	childProcess.execFile('./cli.js', ['www.google.com'], {cwd: __dirname}, function (err, stdout) {
-		t.assert(stdout.trim().length > 0);
+		t.equal(stdout, logSymbols.success + ' Reachable');
 	});
 });
