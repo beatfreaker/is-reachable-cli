349c06bdb9611588029f6c517b0a4b9f2a4ddc17
Chintan Radia <radiachints1512@gmail.com>
2015-08-26 15:08:34 +0530

    added test

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
 'use strict';
 var test = require('tape');
 var childProcess = require('child_process');
-var logSymbols = require('log-symbols');
-
 test('Testing output', function (t) {
-	t.plan(1);
+	t.plan(2);
 	childProcess.execFile('./cli.js', ['www.google.com'], {cwd: __dirname}, function (err, stdout) {
-		t.equal(stdout, logSymbols.success + ' Reachable');
+		if (err) {
+			t.end();
+		}
+		t.equal("✔ Reachable\n", stdout, "Both strings are equal");
+	});
+	childProcess.execFile('./cli.js', ['randowmtext'], {cwd: __dirname}, function (err, stdout) {
+		if (err) {
+			t.end();
+		}
+		t.equal("✖ Not Reachable\n", stdout, "Both strings are equal");
 	});
 });
