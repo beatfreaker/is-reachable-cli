cda3ca7e9323db678ef506887762aa0c70e1d794
Chintan Radia <radiachints1512@gmail.com>
2015-08-26 15:20:17 +0530

    minor tweaks

--- test.js
+++ test.js
@@ -4,15 +4,9 @@ var childProcess = require('child_process');
 test('Testing output', function (t) {
 	t.plan(2);
 	childProcess.execFile('./cli.js', ['www.google.com'], {cwd: __dirname}, function (err, stdout) {
-		if (err) {
-			t.end();
-		}
 		t.equal('✔ Reachable\n', stdout, 'Both strings are equal');
 	});
 	childProcess.execFile('./cli.js', ['randowmtext'], {cwd: __dirname}, function (err, stdout) {
-		if (err) {
-			t.end();
-		}
 		t.equal('✖ Not Reachable\n', stdout, 'Both strings are equal');
 	});
 });
